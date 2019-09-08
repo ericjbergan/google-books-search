9b532bd1d9e03af4d36a29cf11aa7503369d9a8b
Eric Bergan <bootcamp.ejb@gmail.com>
2019-09-08 07:59:01 -0500

    brought in archived files

---
+++ client/src/components/SearchResults/SearchResults.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+function SearchResults(props) {
+    console.log(props.results);
+    return (
+        <ul className="list-group search-results">
+            {props.results.map(result => (
+                <li key={result.accessInfo.id} className="list-group-item">
+                    <div>
+                        <h4>{result.volumeInfo.title}</h4>
+                        <div>
+                            <button
+                                className="searchPageButtons"
+                                onClick={props.clickSave}>
+                                Save
+                                </button>
+                            <a href={result.volumeInfo.infoLink}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <button type="button" className="searchPageButtons">View</button>
+                            </a>
+
+                        </div>
+                    </div>
+                    <p>{result.volumeInfo.authors}</p>
+                    <div>
+                        <img alt="Book" src={result.volumeInfo.imageLinks.thumbnail} className="img-fluid" />
+                        <p>{result.volumeInfo.description}</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default SearchResults;
