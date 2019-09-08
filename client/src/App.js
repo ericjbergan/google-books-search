6a2d5b7c812c9393dc2405530adac226a113d8c9
Eric Bergan <bootcamp.ejb@gmail.com>
2019-09-08 07:53:53 -0500

    just a start

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Wrapper from './components/Wrapper/Wrapper';
-import Search from './pages/Search';
-import Saved from './pages/Saved';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Wrapper >
-          <Route exact path="/" component={Search} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/saved" component={Saved} />
-        </Wrapper>
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
